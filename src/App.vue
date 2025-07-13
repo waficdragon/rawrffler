d3d4742c035bec8050bba6043ae97319988cce4c
yannick <Y.achterstraat@gmail.com>
2025-07-13 13:54:26 +0200

    removed codeformating and unused imports

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,55 @@
 <template>
 
-    <Toast />
+  <Toast />
 
-    <Header />
+  <Header />
 
-    <main class="w-10/10 md:w-9/10 lg:w-8/10 2xl:w-7/10 mx-auto px-[5px]">
+  <main class="w-10/10 md:w-9/10 lg:w-8/10 2xl:w-7/10 mx-auto px-[5px]">
 
-        <Dialogs
-            v-model="dialogs"
-            @addParticipants="addParticipants"
-        />
+    <Dialogs 
+      v-model="dialogs"
+      @addParticipants="addParticipants"
+    />
 
-        <RafflePanel
-            :participants="filteredParticipants"
-            :displayWinnersList
-            v-model:dialogs="dialogs"
-            v-model:numberOfWinners="numberOfWinners"
-        >
-            <RaffleDriver
-                :participants="filteredParticipants"
-                :numberOfWinners
-                v-model:winnersList="winnersList"
-                v-model:displayWinnersList="displayWinnersList"
-            />
-        </RafflePanel>
+    <RafflePanel 
+      :participants="filteredParticipants"
+      :displayWinnersList
+      v-model:dialogs="dialogs" 
+      v-model:numberOfWinners="numberOfWinners"
+    >    
+      <RaffleDriver 
+        :participants="filteredParticipants"
+        :numberOfWinners
+        v-model:winnersList="winnersList"
+        v-model:displayWinnersList="displayWinnersList"
+      />
+    </RafflePanel>
 
-        <RaffleWinners
-            v-if="winnersList.length"
-            v-model:winnersList="winnersList"
-            v-model:displayWinnersList="displayWinnersList"
-            class="mb-5"
-        />
+    <RaffleWinners
+      v-if="winnersList.length"
+      v-model:winnersList="winnersList"
+      v-model:displayWinnersList="displayWinnersList"
+      class="mb-5"
+    />
 
-        <RaffleList
-            v-model:participants="participants"
-            v-model:filteredParticipantsWithSearch="filteredParticipantsWithSearch"
-            @participantRemoved="onParticipantRemoved"
-        >
-            <IconField v-if="participants.length" class="mb-5">
-                <InputIcon class="pi pi-search" />
-                <InputText v-model="search" size="small" placeholder="Filter participants" />
-            </IconField>
-        </RaffleList>
+    <RaffleList 
+      v-model:participants="participants"
+      v-model:filteredParticipantsWithSearch="filteredParticipantsWithSearch"
+      @participantRemoved="onParticipantRemoved"
+    > 
+      <IconField v-if="participants.length" class="mb-5">
+        <InputIcon class="pi pi-search" />
+        <InputText v-model="search" size="small" placeholder="Filter participants" />
+      </IconField>
+    </RaffleList>
 
-    </main>
+  </main>
 
-    <Footer />
+  <Footer />
 </template>
 
 <script setup lang="ts">
-import {ref, computed, watch} from 'vue'
+import {ref, computed} from 'vue'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import RafflePanel from '@/components/RafflePanel.vue'
@@ -66,22 +66,22 @@ const numberOfWinners = ref(1)
 const search = ref('')
 
 const dialogs = ref({
-    openFADialog: false,
-    openFADialogAlt: false,
-    openBskyDialog: false,
-    openManualDialog: false,
-    openJSONDialog: false
+  openFADialog: false,
+  openFADialogAlt: false,
+  openBskyDialog: false,
+  openManualDialog: false,
+  openJSONDialog: false
 })
 
 const normalizeString = (str: string) => str.toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 const filteredParticipants = computed(() => participants.value.filter(
-    user => !winnersList.value.find(winner => winner.id === user.id)
+  user => !winnersList.value.find(winner => winner.id === user.id)
 ))
 const filteredParticipantsWithSearch = computed(() => filteredParticipants.value.filter(
-    user => normalizeString(user.name).includes(normalizeString(search.value))
+  user => normalizeString(user.name).includes(normalizeString(search.value))
 ))
 const addParticipants = (newParticipants: RaffleUser[]) => {
-    participants.value.unshift(...newParticipants)
+  participants.value.unshift(...newParticipants)
 }
 const onParticipantRemoved = () => {
     if (numberOfWinners.value >= filteredParticipants.value.length) {
